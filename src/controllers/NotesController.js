a4d2b6fe635c79d302f8aed27324e00e007f15e4
Guilherme Viana <imluizguilara@hotmail.com>
2024-01-14 11:36:17 -0300

    feat: add note tags to route

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -41,13 +41,33 @@ class NotesController {
 
 		if (tags) {
 			const filterTags = tags.split(',').map((tag) => tag.trim())
-			notes = await knex('tags').whereIn('name', filterTags)
-			return res.json(notes)
+
+			notes = await knex('tags')
+				.select(['notes.id', 'notes.title', 'notes.user_id'])
+				.where('notes.user_id', user_id)
+				.whereLike('notes.title', `%${title}%`)
+				.whereIn('name', filterTags)
+				.innerJoin('notes', 'notes.id', 'tags.note_id') // tabela a conectar, chaves de relação
+				.orderBy('notes.title')
+		} else {
+			notes = await knex('notes')
+				.where({ user_id })
+				.whereLike('title', `%${title}%`)
+				.orderBy('title')
 		}
 
-		notes = await knex('notes').where({ user_id }).whereLike('title', `%${title}%`).orderBy('title')
+		const userTags = await knex('tags').where({ user_id })
 
-		return res.json(notes)
+		const notesWithTags = notes.map((note) => {
+			const noteTags = userTags.filter((tag) => tag.note_id === note.id)
+
+			return {
+				...note,
+				tags: noteTags,
+			}
+		})
+
+		return res.json(notesWithTags)
 	}
 
 	async showUnique(req, res) {
